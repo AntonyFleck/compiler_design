e3d49301ecfef8b5ef010358888717e728f95cef
Anirudha Desai <118076319+AntonyFleck@users.noreply.github.com>
2025-02-28 10:25:33 +0530

    lab6

---
+++ lab6/q2.cu
@@ -0,0 +1,82 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+__device__ int get_gtid()
+{
+    int bng = blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y;
+    int ntpb = blockDim.x * blockDim.y * blockDim.z;
+    int tnb = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y;
+    int gtid = bng * ntpb + tnb;
+    return gtid;
+}
+
+__global__ void ssort(int*A,int*B,int n)
+{
+    int gtid = get_gtid();
+    // if(gtid < n)
+    // {
+    //     int pos = 0;
+    //     int i;
+    //     int data = A[gtid];
+    //     for(i=0;i<n;i++)
+    //     {
+    //         if(A[i] < data || (A[i]==data && i < gtid))
+    //             pos++;
+    //     }
+    //     B[pos]=data;
+    // }
+
+    for(int i=0;i<n;i++){
+        int minIdx = i;
+        for(int j=i+1;j<n;j++){
+            if(A[j]<A[minIdx]){
+                minIdx = j;
+            }
+    }
+    if(gtid==i){
+        int temp = A[i];
+        A[i] = A[minIdx];
+        A[minIdx]=temp;
+    }
+    __syncthreads();
+}
+}
+
+int main()
+{
+    int n;
+    printf("\n220905440");
+    printf("Enter no. of elements: ");
+    scanf("%d",&n);
+
+    int size =  n * sizeof(int);
+    int A[n],B[n];
+    int *d_A,*d_B;
+
+    printf("Enter elements in A: ");
+    for(int i=0; i< n; i++)
+        scanf("%d",&A[i]);
+    
+    cudaMalloc((void**)&d_A , size);
+    cudaMalloc((void**)&d_B , size);
+
+
+    dim3 blk(3,1,2); //i.e 6 threads per block
+    cudaMemcpy(d_A,A,size,cudaMemcpyHostToDevice);
+    ssort<<<ceil(n/6.0),blk>>>(d_A,d_B,n);
+
+    // cudaMemcpy(B,d_B,size,cudaMemcpyDeviceToHost);
+    cudaMemcpy(A,d_A,size,cudaMemcpyDeviceToHost);
+    printf("Sorted array: ");
+	for(int i=0; i<n; i++)
+	{
+		printf("%d ",A[i]);
+	}
+	printf("\n");
+
+	cudaFree(d_A);
+	cudaFree(d_B);
+	return 0;
+
+}
